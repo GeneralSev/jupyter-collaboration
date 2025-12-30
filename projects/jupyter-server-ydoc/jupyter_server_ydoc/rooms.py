643a73b5e6ca863fe0039a1690a0cb93ce40895b
dbansal <dbansal@twd.nl>
2025-12-30 09:20:23 +0100

    jupyter-docprovider v2.2.0+twd10
    - Change immediate save on room creation to only happen if not in read only mode.

--- projects/jupyter-server-ydoc/jupyter_server_ydoc/rooms.py
+++ projects/jupyter-server-ydoc/jupyter_server_ydoc/rooms.py
@@ -184,9 +184,10 @@ class DocumentRoom(YRoom):
             self._emit(LogLevel.INFO, "initialize", "Room initialized")
 
             # Immediately initiate a save action after creation. This is needed to make read only mode work for new notebooks
-            self._saving_document = asyncio.create_task(
-                self._maybe_save_document(self._saving_document)
-            )
+            if not self._read_only:
+                self._saving_document = asyncio.create_task(
+                    self._maybe_save_document(self._saving_document)
+                )
 
     def _emit(self, level: LogLevel, action: str | None = None, msg: str | None = None) -> None:
         data = {"level": level.value, "room": self._room_id, "path": self._file.path}
